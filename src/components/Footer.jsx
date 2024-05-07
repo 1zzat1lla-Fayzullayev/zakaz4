94104df670c043a952186b6d949c2e3a86b3072c
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-07 20:53:15 +0500

    change code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,9 @@ function Footer() {
 							English Essay Writing Help
 						</motion.h1>
 					</motion.div>
-					{/* Hidden */}
+
+
+					{/* Hidden on laptop window */}
 					<motion.div
 						initial={{ opacity: 0, y: 20 }}
 						animate={{ opacity: 1, y: 0 }}
@@ -52,44 +54,52 @@ function Footer() {
 							</li>
 						</ul>
 					</motion.div>
-				</motion.div>
-				<motion.div
-					initial={{ opacity: 0, y: 20 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ delay: 0.6, duration: 0.5 }}
-					className='flex flex-col'
-				>
+					{/* / Hidden on laptop window */}
+
+
+					{/* Footer icons */}
 					<motion.div
 						initial={{ opacity: 0, y: 20 }}
 						animate={{ opacity: 1, y: 0 }}
-						transition={{ delay: 0.8, duration: 0.5 }}
-						className='flex items-center mt-[60px] gap-2'
+						transition={{ delay: 0.6, duration: 0.5 }}
+						className='flex flex-col items-start'
 					>
-						<img
-							src={Images.facebook}
-							alt='facebook'
-							className='cursor-pointer'
-						/>
-						<img
-							src={Images.twitter}
-							alt='twitter'
-							className='cursor-pointer'
-						/>
-						<img
-							src={Images.instagram}
-							alt='instagram'
-							className='cursor-pointer'
-						/>
+						<motion.div
+							initial={{ opacity: 0, y: 20 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ delay: 0.8, duration: 0.5 }}
+							className='flex items-center mt-[60px] gap-2'
+						>
+							<img
+								src={Images.facebook}
+								alt='facebook'
+								className='cursor-pointer'
+							/>
+							<img
+								src={Images.twitter}
+								alt='twitter'
+								className='cursor-pointer'
+							/>
+							<img
+								src={Images.instagram}
+								alt='instagram'
+								className='cursor-pointer'
+							/>
+						</motion.div>
+						<motion.p
+							initial={{ opacity: 0, y: 20 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ delay: 1, duration: 0.5 }}
+							className='text-[#AFB5C0] text-[16px] mt-[10px]'
+						>
+							©2024 English Essay Writing Help
+						</motion.p>
 					</motion.div>
-					<motion.p
-						initial={{ opacity: 0, y: 20 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ delay: 1, duration: 0.5 }}
-						className='text-[#AFB5C0] text-[16px] mt-[10px]'
-					>
-						©2024 English Essay Writing Help
-					</motion.p>
+					{/* Footer icons */}
 				</motion.div>
+
+
+				{/* Hidden on mobile window */}
 				<motion.div
 					initial={{ opacity: 0, y: 20 }}
 					animate={{ opacity: 1, y: 0 }}
@@ -119,6 +129,7 @@ function Footer() {
 						</li>
 					</motion.ul>
 				</motion.div>
+				{/* / Hidden on mobile window */}
 			</div>
 		</>
 	)
